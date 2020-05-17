12b08c99799803ed4afb78ca79f669af37e10405
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-05-17 21:12:59 +0300

    Add files via upload

---
+++ Simon/SimonWindow.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Simon
+{
+    partial class SimonWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redPnl = new System.Windows.Forms.Panel();
+            this.yellowPnl = new System.Windows.Forms.Panel();
+            this.greenPnl = new System.Windows.Forms.Panel();
+            this.bluePnl = new System.Windows.Forms.Panel();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.roundLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // redPnl
+            // 
+            this.redPnl.BackColor = System.Drawing.Color.Red;
+            this.redPnl.Location = new System.Drawing.Point(33, 35);
+            this.redPnl.Name = "redPnl";
+            this.redPnl.Size = new System.Drawing.Size(233, 182);
+            this.redPnl.TabIndex = 0;
+            this.redPnl.Tag = "1";
+            this.redPnl.Click += new System.EventHandler(this.AnyPnl_Click);
+            // 
+            // yellowPnl
+            // 
+            this.yellowPnl.BackColor = System.Drawing.Color.Yellow;
+            this.yellowPnl.Location = new System.Drawing.Point(288, 35);
+            this.yellowPnl.Name = "yellowPnl";
+            this.yellowPnl.Size = new System.Drawing.Size(233, 182);
+            this.yellowPnl.TabIndex = 1;
+            this.yellowPnl.Tag = "2";
+            this.yellowPnl.Click += new System.EventHandler(this.AnyPnl_Click);
+            // 
+            // greenPnl
+            // 
+            this.greenPnl.BackColor = System.Drawing.Color.Lime;
+            this.greenPnl.Location = new System.Drawing.Point(288, 238);
+            this.greenPnl.Name = "greenPnl";
+            this.greenPnl.Size = new System.Drawing.Size(233, 182);
+            this.greenPnl.TabIndex = 3;
+            this.greenPnl.Tag = "4";
+            this.greenPnl.Click += new System.EventHandler(this.AnyPnl_Click);
+            // 
+            // bluePnl
+            // 
+            this.bluePnl.BackColor = System.Drawing.Color.Blue;
+            this.bluePnl.Location = new System.Drawing.Point(33, 238);
+            this.bluePnl.Name = "bluePnl";
+            this.bluePnl.Size = new System.Drawing.Size(233, 182);
+            this.bluePnl.TabIndex = 2;
+            this.bluePnl.Tag = "3";
+            this.bluePnl.Click += new System.EventHandler(this.AnyPnl_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(193, 446);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(175, 75);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Round:";
+            // 
+            // roundLbl
+            // 
+            this.roundLbl.AutoSize = true;
+            this.roundLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.roundLbl.Location = new System.Drawing.Point(91, 433);
+            this.roundLbl.Name = "roundLbl";
+            this.roundLbl.Size = new System.Drawing.Size(16, 20);
+            this.roundLbl.TabIndex = 6;
+            this.roundLbl.Text = "-";
+            // 
+            // SimonWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 544);
+            this.Controls.Add(this.roundLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.greenPnl);
+            this.Controls.Add(this.yellowPnl);
+            this.Controls.Add(this.bluePnl);
+            this.Controls.Add(this.redPnl);
+            this.Name = "SimonWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simon Game";
+            this.Load += new System.EventHandler(this.SimonWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel redPnl;
+        private System.Windows.Forms.Panel yellowPnl;
+        private System.Windows.Forms.Panel greenPnl;
+        private System.Windows.Forms.Panel bluePnl;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label roundLbl;
+    }
+}
+
---
+++ Simon/SimonWindow.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simon
+{
+    public partial class SimonWindow : Form
+    {
+        // preferences
+        // later will be in configuration file
+        const int MAX_NUMBER_OF_RANDOM = 4;
+        const int TIME_BEFORE_CLEAR_NUMBERS = 1000;
+        const int SEQUENCE_MAX_LENGTH = 8;
+        const bool CHEAT_MODE = true;
+        const string WIN_STRING = "You won!! what a great memory!!! you're the best!";
+        const string LOOSE_STRING = "Wrong ..... sorry ....";
+        const string WRONG_NUMBER_STRING = "You chose {0} the correct number was {1}";
+        const string START_USER_INPUT_STRING = "Please start the sequence...";
+        const string DO_YOU_WANT_TO_PLAY_AGAIN_STRING = "Do you want to play again? (Y-yes, N-no)";
+
+        const int RED = 1;
+        const int YELLOW = 2;
+        const int BLUE = 3;
+        const int GREEN = 4;
+
+        // globals
+        int round = 1;
+        int user_sequence_index = 0;
+        int[] sequence_numbers;
+        Random random_generator = new Random();
+        bool wait_for_player = false;
+
+        public SimonWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateRandomSequence()
+        {
+            sequence_numbers = new int[SEQUENCE_MAX_LENGTH];
+
+            for (int i = 0; i < SEQUENCE_MAX_LENGTH; i++)
+            {
+                sequence_numbers[i] = random_generator.Next(1, MAX_NUMBER_OF_RANDOM + 1);
+            }
+        }
+
+        private void DrawEntireSequence()
+        {
+            for (int i = 0; i < sequence_numbers.Length; i++)
+            {
+                //const int RED = 1;
+                //const int YELLOW = 2;
+                //const int BLUE = 3;
+                //const int GREEN = 4;
+
+                switch (sequence_numbers[i])
+                {
+                    case RED:
+                        redPnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        redPnl.BackColor = Color.Red;
+                        break;
+                    case YELLOW:
+                        yellowPnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        yellowPnl.BackColor = Color.Yellow;
+                        break;
+                    case BLUE:
+                        bluePnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        bluePnl.BackColor = Color.Blue;
+                        break;
+                    case GREEN:
+                        Color saveColor = greenPnl.BackColor;
+                        greenPnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        greenPnl.BackColor = saveColor;
+                        break;
+                }
+
+                Thread.Sleep(500);
+            }
+
+        }
+        private void PlayOneNote(int color)
+        {
+            //const int RED = 1;
+            //const int YELLOW = 2;
+            //const int BLUE = 3;
+            //const int GREEN = 4;
+            switch (color)
+            {
+                case RED:
+                    redPnl.BackColor = Color.Black;
+                    Thread.Sleep(500);
+                    redPnl.BackColor = Color.Red;
+                    break;
+                case YELLOW:
+                    yellowPnl.BackColor = Color.Black;
+                    Thread.Sleep(500);
+                    yellowPnl.BackColor = Color.Yellow;
+                    break;
+                case BLUE:
+                    bluePnl.BackColor = Color.Black;
+                    Thread.Sleep(500);
+                    bluePnl.BackColor = Color.Blue;
+                    break;
+                case GREEN:
+                    Color saveColor = greenPnl.BackColor;
+                    greenPnl.BackColor = Color.Black;
+                    Thread.Sleep(500);
+                    greenPnl.BackColor = saveColor;
+                    break;
+            }
+            Thread.Sleep(500);
+
+        }
+        private void PlayCurrentRoundSequence()
+        {
+            if (wait_for_player)
+            {
+                Thread.Sleep(1000);
+            }
+            // round
+            // round = 1, play index 0
+            // round = 2, play index 0 1
+            // round = 3, play index 0 1 2 
+            // sequence_numbers: 2,3,3,2,2,3,1,4,4,4 ...
+            for (int i = 0; i < round; i++)
+            {
+                PlayOneNote(sequence_numbers[i]);
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            GenerateRandomSequence();
+            round = 1;
+            user_sequence_index = 0;
+            wait_for_player = false;
+            // generate new worker
+            //Task.Run(new Action(DrawEntireSequence));
+            roundLbl.Text = "1";
+            Task.Run(new Action(PlayCurrentRoundSequence));
+
+        }
+
+        private void SimonWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AnyPnl_Click(object sender, EventArgs e)
+        {
+            //const int RED = 1;
+            //const int YELLOW = 2;
+            //const int BLUE = 3;
+            //const int GREEN = 4;
+
+            // short way
+            int number_on_panel_tag = Convert.ToInt32((sender as Panel).Tag);
+
+            // long way -- 4 ifs
+            // int number_clicked;
+            //if (sender == redPnl)
+            //{
+            // user clicked red panel == 1
+            // number_clicked = 1;
+            //}
+
+            wait_for_player = true;
+            Task.Run(() => { PlayOneNote(number_on_panel_tag); });
+
+            if (number_on_panel_tag == sequence_numbers[user_sequence_index])
+            {
+                // clicked correct
+                user_sequence_index++;
+
+                //new Thread(() =>
+                //{
+                //    PlayOneNote(number_on_panel_tag);
+                //}).Start();
+                //Thread.Sleep(1000);
+
+                if (user_sequence_index == round)
+                {
+                    user_sequence_index = 0;
+                    round++;
+                    roundLbl.Text = round.ToString();
+                    if (round > SEQUENCE_MAX_LENGTH)
+                    {
+                        roundLbl.Text = "winner !!!!!";
+                        MessageBox.Show("Win", "You won!!!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                    else
+                    {
+                        Task.Run(new Action(PlayCurrentRoundSequence));
+                    }
+                }
+            }
+            else
+            {
+                // clicked wrong!!
+                MessageBox.Show("Game over", "wrong ...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                startBtn_Click(null, null);
+            }
+        }
+    }
+}
